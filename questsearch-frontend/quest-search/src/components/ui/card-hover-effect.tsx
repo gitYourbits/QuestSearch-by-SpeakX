c7829b3620eb537826952344d1312c7983574867
Aditya <aby85365@gmail.com>
2025-01-24 01:54:45 +0530

    minor UI fixes

--- questsearch-frontend/quest-search/src/components/ui/card-hover-effect.tsx
+++ questsearch-frontend/quest-search/src/components/ui/card-hover-effect.tsx
@@ -19,7 +19,7 @@ const HoverEffect = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10 gap-6", // Limited to 2 columns
+        "grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 py-10 gap-6", // Limited to 2 columns
         className
       )}
     >
